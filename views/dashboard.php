29d9d05cea3321a5b7a44d58647327862f00757d
Qasim <adeel@tecfare.com>
2022-09-16 17:20:35 +0500

    Update dashboard.php

--- views/dashboard.php
+++ views/dashboard.php
@@ -1,1 +1,99 @@
-<h2>Dashboard</h2>
+<div class="mt-3">
+    <div class="row row-cols-1 row-cols-md-2 row-cols-xl-3 gx-5">
+        <div class="col">
+            <!-- Collapsible card (users)-->
+            <div class="card card-raised card-collapsible mb-5" id="usersCollapse">
+                <div class="card-header bg-transparent mdc-ripple-upgraded" data-bs-toggle="collapse" data-bs-target="#usersCollapseContent" aria-expanded="true" aria-controls="usersCollapseContent" style="--mdc-ripple-fg-size:197px; --mdc-ripple-fg-scale:1.76313; --mdc-ripple-fg-translate-start:-77.5507px, -87.0644px; --mdc-ripple-fg-translate-end:65.832px, -60.5px;">
+                    <div class="d-flex align-items-center">
+                        <i class="material-icons text-primary">person</i>
+                        <div class="ms-3">
+                            <div class="fs-6 mb-1 fw-500">Users</div>
+                            <div class="small">Add or manage users</div>
+                        </div>
+                    </div>
+                    <i class="material-icons card-header-icon">expand_less</i>
+                </div>
+                <div class="card-body px-0 collapse show" id="usersCollapseContent" data-bs-parent="#usersCollapse" style="">
+                    <div class="nav flex-column">
+                        <a class="nav-link ripple-gray px-3 py-2 mdc-ripple-upgraded" href="#!">Add a user</a>
+                        <a class="nav-link ripple-gray px-3 py-2 mdc-ripple-upgraded" href="#!">Delete a user</a>
+                        <a class="nav-link ripple-gray px-3 py-2 mdc-ripple-upgraded" href="#!">Update a users name or email</a>
+                    </div>
+                </div>
+            </div>
+            <!-- Single card (groups)-->
+            <div class="card card-raised ripple-primary mb-5 mdc-ripple-upgraded" style="--mdc-ripple-fg-size:197px; --mdc-ripple-fg-scale:1.76313; --mdc-ripple-fg-translate-start:64.4791px, -74.8209px; --mdc-ripple-fg-translate-end:65.832px, -60.5px;">
+                <div class="card-body">
+                    <div class="d-flex align-items-center">
+                        <i class="material-icons text-primary">groups</i>
+                        <div class="ms-3">
+                            <div class="fs-6 mb-1 fw-500">Groups</div>
+                            <a class="small stretched-link text-reset text-decoration-none" href="#!">Create or manage groups</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Single card (insights)-->
+            <div class="card card-raised ripple-primary mb-5 mb-xl-0 mdc-ripple-upgraded">
+                <div class="card-body">
+                    <div class="d-flex align-items-center">
+                        <i class="material-icons text-primary">insights</i>
+                        <div class="ms-3">
+                            <div class="fs-6 mb-1 fw-500">Insights</div>
+                            <a class="small stretched-link text-reset text-decoration-none" href="#!">View activity reports</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <!-- Collapsible card (billing)-->
+            <div class="card card-raised card-collapsible mb-5" id="billingCollapse">
+                <div class="card-header bg-transparent mdc-ripple-upgraded" data-bs-toggle="collapse" data-bs-target="#billingCollapseContent" aria-expanded="true" aria-controls="billingCollapseContent" style="--mdc-ripple-fg-size:197px; --mdc-ripple-fg-scale:1.76313; --mdc-ripple-fg-translate-start:207.926px, -74.0172px; --mdc-ripple-fg-translate-end:65.832px, -60.5px;">
+                    <div class="d-flex align-items-center">
+                        <i class="material-icons text-primary">credit_card</i>
+                        <div class="ms-3">
+                            <div class="fs-6 mb-1 fw-500">Billing</div>
+                            <div class="small">Manage subscriptions</div>
+                        </div>
+                    </div>
+                    <i class="material-icons card-header-icon">expand_less</i>
+                </div>
+                <div class="card-body px-0 collapse show" id="billingCollapseContent" data-bs-parent="#billingCollapse" style="">
+                    <div class="nav flex-column">
+                        <a class="nav-link ripple-gray px-3 py-2 mdc-ripple-upgraded" href="#!">Manage subscriptions</a>
+                        <a class="nav-link ripple-gray px-3 py-2 mdc-ripple-upgraded" href="#!">Payment accounts</a>
+                        <a class="nav-link ripple-gray px-3 py-2 mdc-ripple-upgraded" href="#!">Get more services</a>
+                    </div>
+                </div>
+            </div>
+            <!-- Single card (devices)-->
+            <div class="card card-raised ripple-primary mb-5 mdc-ripple-upgraded">
+                <div class="card-body">
+                    <div class="d-flex align-items-center">
+                        <i class="material-icons text-primary">devices</i>
+                        <div class="ms-3">
+                            <div class="fs-6 mb-1 fw-500">Devices</div>
+                            <a class="small stretched-link text-reset text-decoration-none" href="#!">Manage devices and data</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Single card (support)-->
+            <div class="card card-raised ripple-primary mb-5 mb-xl-0 mdc-ripple-upgraded">
+                <div class="card-body">
+                    <div class="d-flex align-items-center">
+                        <i class="material-icons text-primary">support</i>
+                        <div class="ms-3">
+                            <div class="fs-6 mb-1 fw-500">Support</div>
+                            <a class="small stretched-link text-reset text-decoration-none" href="#!">Connect with the support team</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+       
+    
+</div>
+</div>
+   
